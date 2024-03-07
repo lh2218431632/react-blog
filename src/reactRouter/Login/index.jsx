07ffcdb2b303e4cfc21765fa01eb77ce48e9be85
linhao <2218431632@qq.com>
2024-03-07 18:00:25 +0800

    antd主题变量

--- src/reactRouter/Login/index.jsx
+++ src/reactRouter/Login/index.jsx
@@ -1,9 +1,50 @@
-import React from "react";
-
-function Login() {
+import React, { useRef, useEffect } from "react";
+import { Input, Button, ConfigProvider } from "antd";
+import Style from "./index.module.scss";
+function Login(props) {
+  const nameRef = useRef();
+  const passRef = useRef();
+  function handleClick() {
+    console.log("nameRef", nameRef.current.input.value);
+    console.log("passRef", passRef.current.input.value);
+  }
+  useEffect(() => {
+    console.log("nameRef", nameRef.current);
+    console.log("passRef", passRef.current);
+  }, []);
   return (
     <>
-      <div>我是Login页面</div>
+      <div className={Style.container}>
+        <div className={Style.item}>
+          <h1>请登录</h1>
+          <div>登录界面</div>
+          <ConfigProvider
+            theme={{
+              token: {
+                // Seed Token，影响范围大
+                activeBg: "#00b96b",
+                colorBgContainer: "#00b96b",
+              },
+            }}
+          >
+            <Input ref={nameRef} className={Style.ant_input} />
+          </ConfigProvider>
+          <ConfigProvider
+            theme={{
+              token: {
+                // Seed Token，影响范围大
+                activeBg: "#00b96b",
+                colorBgContainer: "#00b96b",
+              },
+            }}
+          >
+            <Input.Password ref={passRef} className={Style.ant_input} />
+          </ConfigProvider>
+          <Button onClick={handleClick} type="primary">
+            登录
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
